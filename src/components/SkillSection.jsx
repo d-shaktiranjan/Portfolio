89ce188b0cdabb729e33092bd52a49f5bf732e1a
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2022-12-05 13:03:32 +0530

    svg icon changes

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -4,7 +4,7 @@ import '../style/skill.css';
 
 export const SkillSection = () => {
     const lanageList = ["Java", "Python", "Java Script", "Node JS", "HTML", "CSS"];
-    const frameworkLibList = ["React", "Express", "Django", "Flask", "Bootstrap"];
+    const frameworkLibList = ["React", "Express JS", "Django", "Flask", "Bootstrap"];
     const techTools = ["Linux", "Git", "Docker", "Mongo DB"];
     return (
         <div className='flex skill-section'>
