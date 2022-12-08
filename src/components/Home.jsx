499c6542cf69944d32b8c1ea2a2b6702b6566e10
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2022-12-09 00:44:03 +0530

    adding image in home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import '../style/home.css';
 import { Link } from 'react-router-dom';
+import myImage from '../static/me.png';
 
 export const Home = () => {
     return (
-        <div className='flex container home min-height'>
-            Hey, This is
-            <span className='main-heading block'>Shakti Ranjan Debata.</span>
-            <span className='main-heading block primary-text-color'>I build Backend things.</span>
-            I'm a Computer Science Engineering student passout from CV Raman Global University.
-            <Link to="/skills" className='primary-btn block'>Explore More <i className="fa-solid fa-arrow-right"></i></Link>
+        <div className='flex container min-height home-section'>
+            <div className="flex home">
+                Hey, This is
+                <span className='main-heading block'>Shakti Ranjan Debata.</span>
+                <span className='main-heading block primary-text-color'>I build Backend things.</span>
+                I'm a Computer Science Engineering student passout from CV Raman Global University.
+                <Link to="/skills" className='primary-btn block'>Explore More <i className="fa-solid fa-arrow-right"></i></Link>
+            </div>
+            <div className="home-image">
+                <img src={myImage} className="my-image" alt="" />
+            </div>
         </div>
     )
 }
