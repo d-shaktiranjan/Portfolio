38f605f599c2c6a6925e0c021e4e0eb74ed8346b
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2022-12-21 21:35:04 +0530

    add option for local image in projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import defaultProjectImage from '../static/project.png';
+import defaultProjectImage from '../static/projectImages/default.png';
 import '../style/project.css';
 
 export const Project = (props) => {
     const projectInfo = props.projectInfo;
     const tools = projectInfo.tools;
+
+    const getProjectImage = () => {
+        if (projectInfo.isLocalImage) {
+            const localImage = require(`../static/projectImages/${projectInfo.projectImage}`);
+            return localImage;
+        }
+        return projectInfo.projectImage || defaultProjectImage;
+    }
+
     return (
         <div className='container flex project-card'>
-            <img className='project-image' src={projectInfo.projectImage || defaultProjectImage} alt="" />
+            <img className='project-image' src={getProjectImage()} alt="" />
             <h3 className='sub-heading project-title accent'>{projectInfo.projectName || "Project Name"}</h3>
             <div className='project-tech'>{tools.join(" | ")}</div>
             <div className='project-details'>
