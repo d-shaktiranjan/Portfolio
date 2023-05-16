d8e9db7d837b43be6a6fc10fcb36aedc286a08c4
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2023-05-16 11:01:45 +0530

    home: company section added & update readme

--- README.md
+++ README.md
@@ -19,6 +19,13 @@ touch .env
 ```
 Then add variables
 ```
+# Home page company section
+VITE_SHOW_COMPANY_SECTION=
+VITE_COMPANY_NAME=
+VITE_COMPANY_URL=
+VITE_COMPANY_ROLE=
+
+# Blog section
 VITE_BLOG_BRANCH=
 VITE_BLOG_BASE_URL=
 ```
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import myImage from '../static/me.png';
 
 export const Home = () => {
+    // env variables
+    const showCompanySection = import.meta.env.VITE_SHOW_COMPANY_SECTION === "true" ? true : false;
+    const companyName = import.meta.env.VITE_COMPANY_NAME;
+    const companyUrl = import.meta.env.VITE_COMPANY_URL;
+    const roleInCompany = import.meta.env.VITE_COMPANY_ROLE;
+
     document.title = "Shakti | Backend Developer";
     return (
         <div className='flex container min-height home-section'>
@@ -10,7 +16,13 @@ export const Home = () => {
                 Hey, This is
                 <span className='main-heading block accent'>Shakti Ranjan Debata.</span>
                 <span className='main-heading block'>I build Backend things.</span>
-                I'm a Computer Science Engineering student passout from CV Raman Global University.
+                <span>
+                    I'm a Computer Science Engineering student passout from CV Raman Global University.
+                </span>
+                {showCompanySection && <span>
+                    Currently working on <a href={companyUrl} target='__blank' className='accent'>{companyName} </a>
+                    as a {roleInCompany}.
+                </span>}
                 <Link to="/skills" className='primary-btn block accent'>Explore Skills <i className="fa-solid fa-arrow-right"></i></Link>
             </div>
             <div className="home-image">
