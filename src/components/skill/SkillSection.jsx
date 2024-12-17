8f590ddf4eb8f44611f381c233887cd05a22b062
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2024-12-17 10:38:12 +0530

    feat: store skills into data->skills.json

--- src/components/skill/SkillSection.jsx
+++ src/components/skill/SkillSection.jsx
@@ -1,41 +1,16 @@
 import { Skills } from "./Skills";
 import "../../style/skill.css";
+import skills from "../../data/skills.json";
 
 export const SkillSection = () => {
   document.title = "Skills | Shakti Ranjan Debata";
 
-  const lanageList = [
-    "Java",
-    "Python",
-    "Java Script",
-    "Node JS",
-    "PHP",
-    "HTML & CSS",
-  ];
-  const frameworkLibList = [
-    "React",
-    "Express JS",
-    "Django",
-    "Flask",
-    "Bootstrap",
-    "Tailwind",
-  ];
-  const techTools = ["Linux", "Git", "Docker", "Mongo DB"];
-  const learningTech = [
-    "React Native",
-    "Typescript",
-    "NEXT.js",
-    "FastAPI",
-    "DSA",
-    "DRF",
-  ];
-
   return (
     <div className="flex skill-section min-height">
-      <Skills list={lanageList} heading="Languages" />
-      <Skills list={frameworkLibList} heading="Framework & libs." />
-      <Skills list={techTools} heading="Tech Tools" />
-      <Skills list={learningTech} heading="Learning" />
+      <Skills list={skills.languageList} heading="Languages" />
+      <Skills list={skills.frameworkLibList} heading="Framework & libs." />
+      <Skills list={skills.techTools} heading="Tech Tools" />
+      <Skills list={skills.learningTech} heading="Learning" />
     </div>
   );
 };
