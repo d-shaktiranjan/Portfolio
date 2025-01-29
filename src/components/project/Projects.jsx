42f4608db8bf58d0e518125b054a2d72509b956e
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2025-01-29 21:48:06 +0530

    feat: `seo` tags added (#15)
    * feat: `seo` tags added
    
    * feat: sitemap.xml added
    
    * feat: robots.txt added
    
    * feat: social meta tags added
    
    * feat: shorten meta desc
    
    * chore: bump version to `1.0.4`

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -4,7 +4,13 @@ import projectData from "../../data/projects.json";
 export const Projects = () => {
   return (
     <div className="container grid project-section">
+      {/* seo tags */}
       <title>Projects | Shakti Ranjan Debata</title>
+      <meta
+        name="description"
+        content="Browse through my backend development projects, showcasing scalable systems, web applications, and API design implementations."
+      />
+
       {projectData.map((item, index) => (
         <ProjectCard projectInfo={item} key={index} />
       ))}
