0fdd245a4d61983e8aca3d61bcc71064b828f3e7
Rahul Choudhury <rchoudhury63@gmail.com>
2025-03-03 20:25:24 +0530

    fix: skills section layout

--- src/components/skill/SkillSection.jsx
+++ src/components/skill/SkillSection.jsx
@@ -4,12 +4,13 @@ import skills from "../../data/skills.json";
 
 export const SkillSection = () => {
   return (
-    <div className="flex skill-section min-height">
+    <div className="min-height skill-container">
       <title>Skills | Shakti Ranjan Debata</title>
-
-      {Object.entries(skills).map(([heading, values]) => (
-        <Skills key={heading} list={values} heading={heading} />
-      ))}
+      <div className="skill-section">
+        {Object.entries(skills).map(([heading, values]) => (
+          <Skills key={heading} list={values} heading={heading} />
+        ))}
+      </div>
     </div>
   );
 };
